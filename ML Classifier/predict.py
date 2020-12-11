80f223ca6db1004511f5017a8ec8980068648a1a
Aryan Rastogi <51237242+aryan-r22@users.noreply.github.com>
2020-12-11 15:16:32 +0530

    Add files via upload

---
+++ ML Classifier/predict.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import pandas as pd
+import os
+
+categories=list(pd.read_csv('train.csv')['Category'].unique())
+
+
+
+if not(os.path.exists("savemodel")):
+	print("Train the model first")
+
+load=tf.saved_model.load("savemodel")
+infer = load.signatures["serving_default"]
+input_sentence=input()
+
+predict_raw=infer(tf.constant([input_sentence]))['dense_4']
+predict=categories[int(tf.argmax(predict_raw,axis=-1))]
+
