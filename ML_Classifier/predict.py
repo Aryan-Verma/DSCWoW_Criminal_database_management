3e46af9ae21a58be76c565945a32d3984fca6713
Rastogi A <aryan.rastogi13@gmail.com>
2020-12-12 10:02:03 +0530

    7th

--- ML_Classifier/predict.py
+++ ML_Classifier/predict.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:ML_Classifier/predict.py
 import tensorflow as tf
 import pandas as pd
 import os
@@ -15,18 +14,19 @@ import os
 	#return(predict)
 #=======
 def predict(input_sentence):
-	categories=list(pd.read_csv('train.csv')['Category'].unique())
-	precit_list =[]
-	if not(os.path.exists("savemodel")):
-	print("Train the model first")
 
-	load=tf.saved_model.load("savemodel")
-	infer = load.signatures["serving_default"]
+    categories=list(pd.read_csv('train.csv')['Category'].unique())
+    precit_list =[]
+    if not(os.path.exists("savemodel")):
+          print("Train the model first")
 
-	for i in input_sentence:
-		predict_raw=infer(tf.constant([i]))['dense_4']
-		predict=categories[int(tf.argmax(predict_raw,axis=-1))]
-		precit_list.append(predict)
-	return precit_list
+    load=tf.saved_model.load("savemodel")
+    infer = load.signatures["serving_default"]
+
+    for i in input_sentence:
+	    predict_raw=infer(tf.constant([i]))['dense_4']
+	    predict=categories[int(tf.argmax(predict_raw,axis=-1))]
+	    precit_list.append(predict)
+    return(precit_list)
 	
->>>>>>> 9733d38a951e3b808e16958801ec8c69d754a721:ML Classifier/predict.py
+
