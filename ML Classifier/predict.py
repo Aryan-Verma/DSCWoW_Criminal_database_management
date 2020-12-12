9c26c6d2ed0a079dcd12d0dae6a1b3fea84e5d39
Smit Patel <46293548+smit25@users.noreply.github.com>
2020-12-12 09:19:26 +0530

    Update predict.py

--- ML Classifier/predict.py
+++ ML Classifier/predict.py
@@ -2,18 +2,17 @@ import tensorflow as tf
 import pandas as pd
 import os
 
-
-def pred(string_s):
-
+def predict(input_sentence):
 	categories=list(pd.read_csv('train.csv')['Category'].unique())
+	precit_list =[]
 	if not(os.path.exists("savemodel")):
-		print("Train the model first")
+	print("Train the model first")
 
 	load=tf.saved_model.load("savemodel")
 	infer = load.signatures["serving_default"]
-	input_sentence=string_s
 
-	predict_raw=infer(tf.constant([input_sentence]))['dense_1']
-	predict=categories[int(tf.argmax(predict_raw,axis=-1))]
-	##PREDICT IS THE FINAL OUTPUT##
-	return(predict)
+	for i in input_sentence:
+		predict_raw=infer(tf.constant([i]))['dense_4']
+		predict=categories[int(tf.argmax(predict_raw,axis=-1))]
+		precit_list.append(predict)
+	return predict_list
