b3e17a3e291529bc68237e4a75326fee8963ef00
Rastogi A <aryan.rastogi13@gmail.com>
2020-12-12 09:24:50 +0530

    Fourth commit

--- ML_Classifier/predict_call.py
+++ ML_Classifier/predict_call.py
@@ -3,11 +3,11 @@ import predict as pr
 
 def get_cat():
                #params
-	cat_descrpt=## function here##
+	cat_descrpt=[]
 
 	list_crimes=[]
 	for x in cat_descrpt:
 		list_crimes.append(pr.pred(x))
 	return(list_crimes)
-
+    
 
