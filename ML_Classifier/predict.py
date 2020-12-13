d10f81a91d032fb0466636e5df3624f31be8760d
Aryan-Verma <46243027+Aryan-Verma@users.noreply.github.com>
2020-12-13 10:28:52 +0530

    Update predict.py

--- ML_Classifier/predict.py
+++ ML_Classifier/predict.py
@@ -1,32 +1,21 @@
 import tensorflow as tf
 import pandas as pd
+import numpy as np
 import os
+def predict2(input_sentence):
+	categories=list(pd.read_csv('train.csv')['Category'].unique())
+	if not(os.path.exists("savemodel")):
+		print("Train the model first")
 
+	predict_list =[]
+	load = tf.saved_model.load('savemodel')
+	infer = load.signatures["serving_default"]
 
-#def pred(string_s):
-#
-###		print("Train the model first")
-#
-#	load=tf.saved_model.load("savemodel")
-#	infer = load.signatures["serving_default"]
-####predict=categories[int(tf.argmax(predict_raw,axis=-1))]
-	##PREDICT IS THE FINAL OUTPUT##
-	#return(predict)
-#=======
-def predict(input_sentence):
-
-    categories=list(pd.read_csv('train.csv')['Category'].unique())
-    precit_list =[]
-    if not(os.path.exists("savemodel")):
-          print("Train the model first")
-
-    load=tf.saved_model.load("savemodel")
-    infer = load.signatures["serving_default"]
-
-    for i in input_sentence:
-	    predict_raw=infer(tf.constant([i]))['dense_4']
-	    predict=categories[int(tf.argmax(predict_raw,axis=-1))]
-	    precit_list.append(predict)
-    return(precit_list)
-	
-
+	#for i in input_sentence:
+	print(input_sentence)
+	for i in input_sentence:
+		predict_raw=infer(tf.constant([i]))['dense_1']
+		predict=categories[int(tf.argmax(predict_raw,axis=-1))]
+		print(predict)
+		predict_list.append(predict)
+	return predict_list
